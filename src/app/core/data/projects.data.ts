9f616372dd8058854cd79a7e46c3199294ce5eda
Ashley <laboryashley@gmail.com>
2025-11-08 01:01:39 +0100

    feature projects done

---
+++ src/app/core/data/projects.data.ts
@@ -0,0 +1,32 @@
+import { Project } from '../models/project';
+
+export const PROJECTS: Project[] = [
+  {
+    title: 'Eshop',
+    description: 'Site e-commerce de plantes.',
+    image: 'https://s.tmimgcdn.com/scr/800x500/301800/pentosh-theme-shopify-pour-magasin-de-jardinage-et-de-plantes_301857-original.jpg',
+    githubUrl: 'https://github.com/AshleyL45/front---odyssea',
+    projectUrl: 'https://ecommerce.example.com',
+    technologies: ['Angular', 'TypeScript', 'Spring Boot', 'MySQL', 'VPS (OVH)'],
+    date: '2025'
+  },
+  {
+    title: 'Portfolio',
+    description: 'Portfolio de mes différents projets.',
+    image: 'https://astro.build/_image?href=https%3A%2F%2Fstorage.googleapis.com%2Fdev-portal-bucket%2Fportfolio.webp&w=1600&h=900&f=webp&dpl=690d329a6c618b0008040d17',
+    githubUrl: 'https://github.com/AshleyL45/portfolio',
+    projectUrl: 'https://aichat.example.com',
+    technologies: ['Angular', 'TypeScript', 'Spring Boot', 'MySQL', 'VPS (OVH)'],
+    date: '2025'
+  },
+  {
+    title: 'Odyssea',
+    description: 'Site de réservation de voyages personnalisés hauts de gamme.',
+    image: 'https://images-wixmp-530a50041672c69d335ba4cf.wixmp.com/templates/image/af39374c417ef2301e5d2ca0735e43d5fb713c78abbd14cb28ca8fd82051959f1661956639772.jpg',
+    frontGithubUrl: 'https://github.com/AshleyL45/portfolio',
+    backGithubUrl: 'https://github.com/AshleyL45/back---odyssea',
+    projectUrl: 'https://ecommerce.example.com',
+    technologies: ['React', 'Node.js', 'TypeScript', 'Spring Boot', 'MySQL'],
+    date: '2024'
+  }
+];
