c54e7d11316dd795b43cb08deaeb591aa8c5f696
Ashley <laboryashley@gmail.com>
2025-11-08 12:24:50 +0100

    internship search modal done + projects images added

--- src/app/features/hero/hero.ts
+++ src/app/features/hero/hero.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {Modal} from '../../shared/components/modal/modal';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
   templateUrl: './hero.html',
   styleUrl: './hero.css',
+  imports: [Modal],
 })
-export class Hero {}
+export class Hero {
+  showModal = signal(false);
+
+  openModal() {
+    this.showModal.set(true);
+  }
+
+  closeModal() {
+    this.showModal.set(false);
+  }
+}
