2c20044790b0d5a08d75da939a607c52469878f8
Ashley <laboryashley@gmail.com>
2025-11-08 14:51:43 +0100

    <p> responsive fixed in hero component border added on project-card component "Voir projet" deleted in Odyssea project card

--- src/app/core/data/projects.data.ts
+++ src/app/core/data/projects.data.ts
@@ -5,9 +5,9 @@ export const PROJECTS: Project[] = [
     title: 'Eshop',
     description: 'Site e-commerce de plantes.',
     image: 'assets/projects/eshop.png',
-    githubUrl: 'https://github.com/AshleyL45/front---odyssea',
-    projectUrl: 'https://ecommerce.example.com',
-    technologies: ['Angular', 'TypeScript', 'Spring Boot', 'MySQL', 'VPS (OVH)'],
+    githubUrl: 'https://github.com/AshleyL45/e-shop',
+    projectUrl: 'https://ashleydev.fr/',
+    technologies: ['Angular', 'TypeScript', 'VPS (OVH)'],
     date: '2025'
   },
   {
@@ -15,7 +15,7 @@ export const PROJECTS: Project[] = [
     description: 'Portfolio de mes différents projets.',
     image: 'assets/projects/portfolio.png',
     githubUrl: 'https://github.com/AshleyL45/portfolio',
-    projectUrl: 'https://aichat.example.com',
+    projectUrl: 'https://ashleylabory.fr/',
     technologies: ['Angular', 'TypeScript', 'Spring Boot', 'MySQL', 'VPS (OVH)'],
     date: '2025'
   },
@@ -25,7 +25,6 @@ export const PROJECTS: Project[] = [
     image: 'assets/projects/odyssea.png',
     frontGithubUrl: 'https://github.com/AshleyL45/portfolio',
     backGithubUrl: 'https://github.com/AshleyL45/back---odyssea',
-    projectUrl: 'https://ecommerce.example.com',
     technologies: ['React', 'Node.js', 'TypeScript', 'Spring Boot', 'MySQL'],
     date: '2024'
   }
